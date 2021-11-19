8dbad056ea42dacb260e9d30955d8dfb008d412b
Pe-Guedss <pedro.og2002@gmail.com>
2021-11-19 15:15:09 -0300

    Projeto para leitura e exibição de .txt na web.

---
+++ 03-importing-txt-files/fileProcessor.ts
@@ -0,0 +1,68 @@
+function createpElement(text:string){
+    var pEl = document.createElement('p');
+    pEl.textContent = text;
+    return pEl;
+}
+
+function createUlElement(num:number){
+    var ulElement = document.createElement('ul');
+    ulElement.className = `child-elements${num}`;
+    return ulElement;
+}
+
+function createLiElement(text:string){
+    var liElement = document.createElement('li');
+    liElement.textContent = text;
+    return liElement;
+}
+
+document.getElementById('txtfile').onchange = function(){
+  
+    var file = document.querySelector('input').files[0];
+
+    var reader = new FileReader();
+    reader.onload = function(){
+        var lines = (this.result as string).split('\n');
+        for(var line = 0; line < lines.length; line++){
+            var fields = lines[line].split(";");
+            if (fields.length - 1 != 20 && line != lines.length - 1) {
+                // console.log(lines[line]);
+                // console.log("Erro ao abrir o arquivo!\nNão há campos o suficiente.\n\n");
+                alert(`Erro ao abrir o arquivo!
+
+Não há campos o suficiente. Linha: ${line + 1}.`);
+                break;
+            }
+        }
+
+        let hierarchy = document.querySelector('.hierarchy');
+        var num:number = 0;
+
+        for(var line = 0; line < lines.length; line++){
+            fields = lines[line].split(";");
+
+            if (fields[19].replace(" ", "") == 'P') {
+                hierarchy.appendChild(createpElement(lines[line]));
+
+                var ulElement = document.createElement('ul');
+                ulElement.className = `child-elements`;
+                hierarchy.appendChild(ulElement);
+            } 
+
+            else if (fields[19].replace(" ", "") == 'M') {
+                num++;
+                hierarchy.lastElementChild.appendChild(createLiElement(lines[line]));
+                hierarchy.lastElementChild.appendChild(createUlElement(num));
+            } 
+
+            else if (fields[19].replace(" ", "") == 'Q'){
+                hierarchy.lastElementChild.appendChild(createpElement(lines[line]));
+            }
+
+            else {
+                hierarchy.lastElementChild.lastElementChild.appendChild(createLiElement(lines[line]));
+            }
+        }
+    };
+    reader.readAsText(file);
+};
